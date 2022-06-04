cec2a96a3b66825a9c19810138db151a2c4dae5b
Subhash Bhandari <73028876+user5055@users.noreply.github.com>
2022-06-04 20:34:19 +0545

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,1 +1,3 @@
-print("Hello World");
+import time
+print("Hello There")
+print("\n"+time.sleep(5)+"This"+time.sleep(2)+"is"+time.sleep(2)+"my"+time.sleep(2)+"first"+time.sleep(2)+"repository"+time.sleep(2)+"!"+time.sleep(2)+"!"+time.sleep(2)+"!")
